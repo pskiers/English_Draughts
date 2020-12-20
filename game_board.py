33dc46690736e14a34a5b429315d457d44f27c18
pawel <pawelsk.vp.pl@gmial.com>
2020-12-20 23:12:51 +0100

    added tests and fixed bugs

--- game_board.py
+++ game_board.py
@@ -2,7 +2,7 @@ from Errors import (
     BoardRowCountError,
     RowLengthError,
     PieceOnAWhiteSquareError,
-    NotAllowedThingOnABoardError,
+    NotAllowedThingOnTheBoardError,
     NonIterableRowError,
     NonIterableBoardError
 )
@@ -23,16 +23,20 @@ class game_board:
         sets value of board atribute
     """
     def __init__(self, board=None):
-        self._board = self.set_board(board)
+        self.set_board(board)
 
     def __str__(self):
-        return str(self.board())
+        board = ''
+        for row in self.board():
+            board += str(row)
+            board += '\n'
+        return board
 
     def board(self):
         return self._board
 
-    def set_board(self, board):
-        if not board:
+    def set_board(self, newboard):
+        if not newboard:
             self._board = [
                 [' ', 'x', ' ', 'x', ' ', 'x', ' ', 'x'],
                 ['x', ' ', 'x', ' ', 'x', ' ', 'x', ' '],
@@ -44,23 +48,24 @@ class game_board:
                 ['o', ' ', 'o', ' ', 'o', ' ', 'o', ' '],
             ]
         else:
-            if len(board) != 8:
-                raise BoardRowCountError()
+            try:
+                if len(newboard) != 8:
+                    raise BoardRowCountError()
+            except TypeError:
+                raise NonIterableBoardError()
             else:
-                try:
-                    for i in range(1,9):
-                        if len(board[i]) != 8:
+                for i in range(8):
+                    try:
+                        if len(newboard[i]) != 8:
                             raise RowLengthError()
-                        try:
-                            for j in range(1, 9):
-                                if i % 2 == j % 2:
-                                    if board[i][j] != ' ':
-                                        raise PieceOnAWhiteSquareError()
-                                else:
-                                    PosSigns = [' ', 'x', 'X', 'o', 'O']
-                                    if board[i][j] not in PosSigns:
-                                        raise NotAllowedThingOnABoardError()
-                        except TypeError:
-                            raise NonIterableRowError()
-                except TypeError:
-                    raise NonIterableBoardError()
+                    except TypeError:
+                        raise NonIterableRowError()
+                    for j in range(8):
+                        if i % 2 == j % 2:
+                            if newboard[i][j] != ' ':
+                                raise PieceOnAWhiteSquareError()
+                        else:
+                            PosSigns = [' ', 'x', 'X', 'o', 'O']
+                            if newboard[i][j] not in PosSigns:
+                                raise NotAllowedThingOnTheBoardError()
+                self._board = newboard
--- test_game_board.py
+++ test_game_board.py
@@ -1,3 +1,12 @@
+import pytest
+from Errors import (
+    BoardRowCountError,
+    RowLengthError,
+    PieceOnAWhiteSquareError,
+    NotAllowedThingOnTheBoardError,
+    NonIterableRowError,
+    NonIterableBoardError
+)
 from game_board import game_board
 
 
@@ -14,3 +23,98 @@ def test_createDefaultGameBoard():
         ['o', ' ', 'o', ' ', 'o', ' ', 'o', ' '],
     ]
     assert board.board() == standardboard
+
+
+def test_createGameBoard():
+    board = [
+        [' ', 'x', ' ', 'x', ' ', 'x', ' ', 'x'],
+        ['x', ' ', 'x', ' ', 'x', ' ', 'x', ' '],
+        [' ', ' ', ' ', 'x', ' ', 'x', ' ', 'x'],
+        [' ', ' ', 'x', ' ', ' ', ' ', ' ', ' '],
+        [' ', 'o', ' ', ' ', ' ', ' ', ' ', ' '],
+        ['o', ' ', ' ', ' ', 'o', ' ', 'o', ' '],
+        [' ', 'o', ' ', 'o', ' ', 'o', ' ', 'o'],
+        ['o', ' ', 'o', ' ', 'o', ' ', 'o', ' '],
+    ]
+    somegame = game_board(board)
+    assert somegame.board() == board
+
+
+def test_createBadGameBoard():
+    board = [
+        [' ', 'x', ' ', 'x', ' ', 'x', ' ', 'x'],
+        ['x', ' ', 'x', ' ', 'x', ' ', 'x', ' '],
+        [' ', ' ', ' ', 'x', ' ', 'x', ' ', 'x'],
+        [' ', ' ', 'x', ' ', ' ', ' ', ' ', ' '],
+        [' ', 'o', ' ', ' ', ' ', ' ', ' ', ' '],
+        ['o', ' ', ' ', ' ', 'o', ' ', 'o', ' '],
+        [' ', 'o', ' ', 'o', ' ', 'o', ' ', 'o'],
+    ]
+    with pytest.raises(BoardRowCountError):
+        game_board(board)
+
+
+def test_createBadGameBoard2():
+    board = [
+        [' ', 'x', ' ', 'x', ' ', 'x', ' '],
+        ['x', ' ', 'x', ' ', 'x', ' ', 'x', ' '],
+        [' ', ' ', ' ', 'x', ' ', 'x', ' ', 'x'],
+        [' ', ' ', 'x', ' ', ' ', ' ', ' ', ' '],
+        [' ', 'o', ' ', ' ', ' ', ' ', ' ', ' '],
+        ['o', ' ', ' ', ' ', 'o', ' ', 'o', ' '],
+        [' ', 'o', ' ', 'o', ' ', 'o', ' ', 'o'],
+        ['o', ' ', 'o', ' ', 'o', ' ', 'o', ' '],
+    ]
+    with pytest.raises(RowLengthError):
+        game_board(board)
+
+
+def test_createBadGameBoard3():
+    board = [
+        [' ', 'x', ' ', 'x', ' ', 'x', 'x', 'x'],
+        ['x', ' ', 'x', ' ', 'x', ' ', 'x', ' '],
+        [' ', ' ', ' ', 'x', ' ', 'x', ' ', 'x'],
+        [' ', ' ', 'x', ' ', ' ', ' ', ' ', ' '],
+        [' ', 'o', ' ', ' ', ' ', ' ', ' ', ' '],
+        ['o', ' ', ' ', ' ', 'o', ' ', 'o', ' '],
+        [' ', 'o', ' ', 'o', ' ', 'o', ' ', 'o'],
+        ['o', ' ', 'o', ' ', 'o', ' ', 'o', ' '],
+    ]
+    with pytest.raises(PieceOnAWhiteSquareError):
+        game_board(board)
+
+
+def test_createBadGameBoard4():
+    board = [
+        [' ', 'x', ' ', 'x', ' ', '1', ' ', 'x'],
+        ['x', ' ', 'x', ' ', 'x', ' ', 'x', ' '],
+        [' ', ' ', ' ', 'x', ' ', 'x', ' ', 'x'],
+        [' ', ' ', 'x', ' ', ' ', ' ', ' ', ' '],
+        [' ', 'o', ' ', ' ', ' ', ' ', ' ', ' '],
+        ['o', ' ', ' ', ' ', 'o', ' ', 'o', ' '],
+        [' ', 'o', ' ', 'o', ' ', 'o', ' ', 'o'],
+        ['o', ' ', 'o', ' ', 'o', ' ', 'o', ' '],
+    ]
+    with pytest.raises(NotAllowedThingOnTheBoardError):
+        game_board(board)
+
+
+def test_createBadGameBoard5():
+    board = 2
+    with pytest.raises(NonIterableBoardError):
+        game_board(board)
+
+
+def test_createBadGameBoard6():
+    board = [
+        1,
+        ['x', ' ', 'x', ' ', 'x', ' ', 'x', ' '],
+        [' ', ' ', ' ', 'x', ' ', 'x', ' ', 'x'],
+        [' ', ' ', 'x', ' ', ' ', ' ', ' ', ' '],
+        [' ', 'o', ' ', ' ', ' ', ' ', ' ', ' '],
+        ['o', ' ', ' ', ' ', 'o', ' ', 'o', ' '],
+        [' ', 'o', ' ', 'o', ' ', 'o', ' ', 'o'],
+        ['o', ' ', 'o', ' ', 'o', ' ', 'o', ' '],
+    ]
+    with pytest.raises(NonIterableRowError):
+        game_board(board)
