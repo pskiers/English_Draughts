54757598366204ef7498dbb2ad4aa5808ab85573
pawel <pawelsk.vp.pl@gmial.com>
2021-01-05 19:41:06 +0100

    moved makeamove from class game to class gameboard

--- game_board.py
+++ game_board.py
@@ -4,8 +4,15 @@ from Errors import (
     PieceOnAWhiteSquareError,
     NotAllowedThingOnTheBoardError,
     NonIterableRowError,
-    NonIterableBoardError
+    NonIterableBoardError,
+    SquareTakenError,
+    NoPieceOnTheSquareError,
+    CapturingNothingError,
+    CapturingYourOwnPiceError,
+    NotAMoveError,
+    PawnMovingBackwardsError,
 )
+from moves import push, capture
 
 
 class game_board:
@@ -77,3 +84,127 @@ class game_board:
                             if newboard[i][j] not in PosSigns:
                                 raise NotAllowedThingOnTheBoardError()
                 self._board = newboard
+
+    def make_a_move(self, move):
+        """
+        make_a_move method makes a move on the game's gameboard
+
+        :param move: move that is to be made
+        :param type: class push or class capture
+        """
+        if type(move).__name__ == 'push':
+            self.push_or_capture(move, 0)
+        elif type(move).__name__ == 'capture':
+            self.push_or_capture(move, 1)
+        else:
+            raise NotAMoveError()
+
+    def remove_captured_piece(self, x1, y1, x2, y2):
+        """
+        remove_captured_piece method removes captured piece from the board
+        :param x1: starting row of the capturing piece
+        :param type: int
+        :param y1: starting column of the capturing piece
+        :param type: int
+        :param x2: destination row of the capturing piece
+        :param type: int
+        :param y2: destination column of the capturing piece
+        :param type: int
+        """
+        rmx = int((x2 + x1) / 2)
+        rmy = int((y2 + y1) / 2)
+        captured = self.board()[rmx][rmy]
+        if captured == ' ':
+            raise CapturingNothingError()
+        elif captured.lower() == self.board()[x1][y1].lower():
+            raise CapturingYourOwnPiceError()
+        else:
+            self.board()[rmx][rmy] = ' '
+
+    def push_or_capture(self, move, op: bool):
+        """
+        push_or_capture method identifies which function should be used to
+        make the move
+
+        :param move: move to be made
+        :param type: class push or capture
+        :param op: indicates whether move is a capture move
+        :param op: bool
+        """
+        x1, y1 = move.origin()
+        x2, y2 = move.destination()
+        if self.board()[x1][y1] == 'o':
+            self.move_an_o(x1, y1, x2, y2, op)
+        elif self.board()[x1][y1] == 'x':
+            self.move_an_x(x1, y1, x2, y2, op)
+        elif self.board()[x1][y1] == ' ':
+            raise NoPieceOnTheSquareError()
+        else:
+            self.just_move(x1, y1, x2, y2, op)
+
+    def just_move(self, x1, y1, x2, y2, op: bool):
+        """
+        just_move method simply makes a move
+
+        :param x1: starting row of the moving piece
+        :param type: int
+        :param y1: starting column of the moving piece
+        :param type: int
+        :param x2: destination row of the moving piece
+        :param type: int
+        :param y2: destination column of the moving piece
+        :param type: int
+        :param op: indicates whether move is a capture move
+        :param op: bool
+        """
+        if self.board()[x2][y2] == ' ':
+            if op == 1:
+                self.remove_captured_piece(x1, y1, x2, y2)
+            self.board()[x2][y2] = self.board()[x1][y1]
+            self.board()[x1][y1] = ' '
+        else:
+            raise SquareTakenError()
+
+    def move_an_o(self, x1, y1, x2, y2, op: bool):
+        """
+        move_an_o method moves 'o' type pieces
+
+        :param x1: starting row of the moving piece
+        :param type: int
+        :param y1: starting column of the moving piece
+        :param type: int
+        :param x2: destination row of the moving piece
+        :param type: int
+        :param y2: destination column of the moving piece
+        :param type: int
+        :param op: indicates whether move is a capture move
+        :param op: bool
+        """
+        if x1 < x2:
+            raise PawnMovingBackwardsError()
+        else:
+            if x2 == 0:
+                self.board()[x1][y1] = 'O'
+            self.just_move(x1, y1, x2, y2, op)
+
+    def move_an_x(self, x1, y1, x2, y2, op: bool):
+        """
+        move_an_x method moves 'x' type pieces
+
+        :param x1: starting row of the moving piece
+        :param type: int
+        :param y1: starting column of the moving piece
+        :param type: int
+        :param x2: destination row of the moving piece
+        :param type: int
+        :param y2: destination column of the moving piece
+        :param type: int
+        :param op: indicates whether move is a capture move
+        :param op: bool
+        """
+        if x1 > x2:
+            raise PawnMovingBackwardsError()
+        else:
+            if x2 == 7:
+                self.board()[x1][y1] = 'X'
+            self.just_move(x1, y1, x2, y2, op)
--- test_game_board.py
+++ test_game_board.py
@@ -1,11 +1,18 @@
 import pytest
+from moves import push, capture
 from Errors import (
     BoardRowCountError,
     RowLengthError,
     PieceOnAWhiteSquareError,
     NotAllowedThingOnTheBoardError,
     NonIterableRowError,
-    NonIterableBoardError
+    NonIterableBoardError,
+    PawnMovingBackwardsError,
+    SquareTakenError,
+    CapturingNothingError,
+    CapturingYourOwnPiceError,
+    NoPieceOnTheSquareError,
+    NotAMoveError,
 )
 from game_board import game_board
 
@@ -118,3 +125,371 @@ def test_createBadGameBoard6():
     ]
     with pytest.raises(NonIterableRowError):
         game_board(board)
+
+
+def test_just_move():
+    board = game_board()
+    board.just_move(5, 0, 4, 1, 0)
+    board2 = [
+        [' ', 'x', ' ', 'x', ' ', 'x', ' ', 'x'],
+        ['x', ' ', 'x', ' ', 'x', ' ', 'x', ' '],
+        [' ', 'x', ' ', 'x', ' ', 'x', ' ', 'x'],
+        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+        [' ', 'o', ' ', ' ', ' ', ' ', ' ', ' '],
+        [' ', ' ', 'o', ' ', 'o', ' ', 'o', ' '],
+        [' ', 'o', ' ', 'o', ' ', 'o', ' ', 'o'],
+        ['o', ' ', 'o', ' ', 'o', ' ', 'o', ' '],
+    ]
+    assert board.board() == board2
+
+
+def test_just_bad_move():
+    board = game_board()
+    with pytest.raises(SquareTakenError):
+        board.just_move(6, 1, 5, 0, 0)
+
+
+def test_standard_move_an_o():
+    board = game_board()
+    board.move_an_o(5, 0, 4, 1, 0)
+    board2 = [
+        [' ', 'x', ' ', 'x', ' ', 'x', ' ', 'x'],
+        ['x', ' ', 'x', ' ', 'x', ' ', 'x', ' '],
+        [' ', 'x', ' ', 'x', ' ', 'x', ' ', 'x'],
+        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+        [' ', 'o', ' ', ' ', ' ', ' ', ' ', ' '],
+        [' ', ' ', 'o', ' ', 'o', ' ', 'o', ' '],
+        [' ', 'o', ' ', 'o', ' ', 'o', ' ', 'o'],
+        ['o', ' ', 'o', ' ', 'o', ' ', 'o', ' '],
+    ]
+    assert board.board() == board2
+
+
+def test_move_an_o_to_king():
+    board1 = [
+        [' ', ' ', ' ', 'x', ' ', 'x', ' ', 'x'],
+        ['o', ' ', 'x', ' ', 'x', ' ', 'x', ' '],
+        [' ', ' ', ' ', 'x', ' ', 'x', ' ', 'x'],
+        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+        [' ', ' ', 'o', ' ', 'o', ' ', 'o', ' '],
+        [' ', 'o', ' ', 'o', ' ', 'o', ' ', 'o'],
+        ['o', ' ', 'o', ' ', 'o', ' ', 'o', ' '],
+    ]
+    board = game_board(board1)
+    board.move_an_o(1, 0, 0, 1, 0)
+    board2 = [
+        [' ', 'O', ' ', 'x', ' ', 'x', ' ', 'x'],
+        [' ', ' ', 'x', ' ', 'x', ' ', 'x', ' '],
+        [' ', ' ', ' ', 'x', ' ', 'x', ' ', 'x'],
+        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+        [' ', ' ', 'o', ' ', 'o', ' ', 'o', ' '],
+        [' ', 'o', ' ', 'o', ' ', 'o', ' ', 'o'],
+        ['o', ' ', 'o', ' ', 'o', ' ', 'o', ' '],
+    ]
+    assert board.board() == board2
+
+
+def test_bad_move_an_o():
+    board2 = [
+        [' ', 'x', ' ', 'x', ' ', 'x', ' ', 'x'],
+        ['x', ' ', 'x', ' ', 'x', ' ', 'x', ' '],
+        [' ', 'x', ' ', 'x', ' ', 'x', ' ', 'x'],
+        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+        [' ', 'o', ' ', ' ', ' ', ' ', ' ', ' '],
+        [' ', ' ', 'o', ' ', 'o', ' ', 'o', ' '],
+        [' ', 'o', ' ', 'o', ' ', 'o', ' ', 'o'],
+        ['o', ' ', 'o', ' ', 'o', ' ', 'o', ' '],
+    ]
+    board = game_board(board2)
+    with pytest.raises(PawnMovingBackwardsError):
+        board.move_an_o(4, 1, 5, 0, 0)
+
+
+def test_standard_move_an_x():
+    board = game_board()
+    board.move_an_x(2, 1, 3, 0, 0)
+    board2 = [
+        [' ', 'x', ' ', 'x', ' ', 'x', ' ', 'x'],
+        ['x', ' ', 'x', ' ', 'x', ' ', 'x', ' '],
+        [' ', ' ', ' ', 'x', ' ', 'x', ' ', 'x'],
+        ['x', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+        ['o', ' ', 'o', ' ', 'o', ' ', 'o', ' '],
+        [' ', 'o', ' ', 'o', ' ', 'o', ' ', 'o'],
+        ['o', ' ', 'o', ' ', 'o', ' ', 'o', ' '],
+    ]
+    assert board.board() == board2
+
+
+def test_move_an_x_to_king():
+    board1 = [
+        [' ', ' ', ' ', 'x', ' ', 'x', ' ', 'x'],
+        ['o', ' ', 'x', ' ', 'x', ' ', 'x', ' '],
+        [' ', ' ', ' ', 'x', ' ', 'x', ' ', 'x'],
+        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+        [' ', ' ', 'o', ' ', 'o', ' ', 'o', ' '],
+        [' ', 'x', ' ', 'o', ' ', 'o', ' ', 'o'],
+        [' ', ' ', 'o', ' ', 'o', ' ', 'o', ' '],
+    ]
+    board = game_board(board1)
+    board.move_an_x(6, 1, 7, 0, 0)
+    board2 = [
+        [' ', ' ', ' ', 'x', ' ', 'x', ' ', 'x'],
+        ['o', ' ', 'x', ' ', 'x', ' ', 'x', ' '],
+        [' ', ' ', ' ', 'x', ' ', 'x', ' ', 'x'],
+        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+        [' ', ' ', 'o', ' ', 'o', ' ', 'o', ' '],
+        [' ', ' ', ' ', 'o', ' ', 'o', ' ', 'o'],
+        ['X', ' ', 'o', ' ', 'o', ' ', 'o', ' '],
+    ]
+    assert board.board() == board2
+
+
+def test_bad_move_an_x():
+    board2 = [
+        [' ', 'x', ' ', 'x', ' ', 'x', ' ', 'x'],
+        ['x', ' ', 'x', ' ', 'x', ' ', 'x', ' '],
+        [' ', ' ', ' ', 'x', ' ', 'x', ' ', 'x'],
+        ['x', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+        ['o', ' ', 'o', ' ', 'o', ' ', 'o', ' '],
+        [' ', 'o', ' ', 'o', ' ', 'o', ' ', 'o'],
+        ['o', ' ', 'o', ' ', 'o', ' ', 'o', ' '],
+    ]
+    board = game_board(board2)
+    with pytest.raises(PawnMovingBackwardsError):
+        board.move_an_x(3, 0, 2, 1, 0)
+
+
+def test_remove_captured_piece():
+    board1 = [
+        [' ', ' ', ' ', 'x', ' ', 'x', ' ', 'x'],
+        ['o', ' ', 'x', ' ', 'x', ' ', 'x', ' '],
+        [' ', ' ', ' ', 'x', ' ', 'x', ' ', 'x'],
+        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+        [' ', ' ', 'o', ' ', 'o', ' ', 'o', ' '],
+        [' ', 'x', ' ', 'o', ' ', 'o', ' ', 'o'],
+        [' ', ' ', 'o', ' ', 'o', ' ', 'o', ' '],
+    ]
+    board = game_board(board1)
+    board.remove_captured_piece(7, 2, 5, 0)
+    board2 = [
+        [' ', ' ', ' ', 'x', ' ', 'x', ' ', 'x'],
+        ['o', ' ', 'x', ' ', 'x', ' ', 'x', ' '],
+        [' ', ' ', ' ', 'x', ' ', 'x', ' ', 'x'],
+        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+        [' ', ' ', 'o', ' ', 'o', ' ', 'o', ' '],
+        [' ', ' ', ' ', 'o', ' ', 'o', ' ', 'o'],
+        [' ', ' ', 'o', ' ', 'o', ' ', 'o', ' '],
+    ]
+    assert board.board() == board2
+
+
+def test_bad_remove_captured_piece():
+    board2 = [
+        [' ', 'x', ' ', 'x', ' ', 'x', ' ', 'x'],
+        ['x', ' ', 'x', ' ', 'x', ' ', 'x', ' '],
+        [' ', ' ', ' ', 'x', ' ', 'x', ' ', 'x'],
+        ['x', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+        ['o', ' ', 'o', ' ', 'o', ' ', 'o', ' '],
+        [' ', 'o', ' ', 'o', ' ', 'o', ' ', 'o'],
+        ['o', ' ', 'o', ' ', 'o', ' ', 'o', ' '],
+    ]
+    board = game_board(board2)
+    with pytest.raises(CapturingNothingError):
+        board.remove_captured_piece(5, 0, 3, 2)
+
+
+def test_bad_remove_captured_piece2():
+    board2 = [
+        [' ', 'x', ' ', 'x', ' ', 'x', ' ', 'x'],
+        ['x', ' ', 'x', ' ', 'x', ' ', 'x', ' '],
+        [' ', ' ', ' ', 'x', ' ', 'x', ' ', 'x'],
+        ['x', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+        ['o', ' ', 'o', ' ', 'o', ' ', 'o', ' '],
+        [' ', 'o', ' ', 'o', ' ', 'o', ' ', 'o'],
+        ['o', ' ', 'o', ' ', 'o', ' ', 'o', ' '],
+    ]
+    board = game_board(board2)
+    with pytest.raises(CapturingYourOwnPiceError):
+        board.remove_captured_piece(6, 1, 4, 3)
+
+
+def test_PUSH_or_capture_o():
+    board = game_board()
+    move = push((5, 0), (4, 1))
+    board.push_or_capture(move, 0)
+    board2 = [
+        [' ', 'x', ' ', 'x', ' ', 'x', ' ', 'x'],
+        ['x', ' ', 'x', ' ', 'x', ' ', 'x', ' '],
+        [' ', 'x', ' ', 'x', ' ', 'x', ' ', 'x'],
+        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+        [' ', 'o', ' ', ' ', ' ', ' ', ' ', ' '],
+        [' ', ' ', 'o', ' ', 'o', ' ', 'o', ' '],
+        [' ', 'o', ' ', 'o', ' ', 'o', ' ', 'o'],
+        ['o', ' ', 'o', ' ', 'o', ' ', 'o', ' '],
+    ]
+    assert board.board() == board2
+
+
+def test_push_or_CAPTURE_o():
+    board1 = [
+        [' ', ' ', ' ', 'x', ' ', 'x', ' ', 'x'],
+        ['o', ' ', 'x', ' ', 'x', ' ', 'x', ' '],
+        [' ', ' ', ' ', 'x', ' ', 'x', ' ', 'x'],
+        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+        [' ', ' ', 'o', ' ', 'o', ' ', 'o', ' '],
+        [' ', 'x', ' ', 'o', ' ', 'o', ' ', 'o'],
+        [' ', ' ', 'o', ' ', 'o', ' ', 'o', ' '],
+    ]
+    board = game_board(board1)
+    move = capture((7, 2), (5, 0))
+    board.push_or_capture(move, 1)
+    board2 = [
+        [' ', ' ', ' ', 'x', ' ', 'x', ' ', 'x'],
+        ['o', ' ', 'x', ' ', 'x', ' ', 'x', ' '],
+        [' ', ' ', ' ', 'x', ' ', 'x', ' ', 'x'],
+        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+        ['o', ' ', 'o', ' ', 'o', ' ', 'o', ' '],
+        [' ', ' ', ' ', 'o', ' ', 'o', ' ', 'o'],
+        [' ', ' ', ' ', ' ', 'o', ' ', 'o', ' '],
+    ]
+    assert board.board() == board2
+
+
+def test_PUSH_or_capture_x():
+    board = game_board()
+    move = push((2, 1), (3, 0))
+    board.push_or_capture(move, 0)
+    board2 = [
+        [' ', 'x', ' ', 'x', ' ', 'x', ' ', 'x'],
+        ['x', ' ', 'x', ' ', 'x', ' ', 'x', ' '],
+        [' ', ' ', ' ', 'x', ' ', 'x', ' ', 'x'],
+        ['x', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+        ['o', ' ', 'o', ' ', 'o', ' ', 'o', ' '],
+        [' ', 'o', ' ', 'o', ' ', 'o', ' ', 'o'],
+        ['o', ' ', 'o', ' ', 'o', ' ', 'o', ' '],
+    ]
+    assert board.board() == board2
+
+
+def test_push_or_CAPTURE_x():
+    board1 = [
+        [' ', ' ', ' ', 'x', ' ', 'x', ' ', 'x'],
+        [' ', ' ', 'x', ' ', 'x', ' ', 'x', ' '],
+        [' ', 'o', ' ', 'x', ' ', 'x', ' ', 'x'],
+        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+        [' ', ' ', 'o', ' ', 'o', ' ', 'o', ' '],
+        [' ', 'x', ' ', 'o', ' ', 'o', ' ', 'o'],
+        [' ', ' ', 'o', ' ', 'o', ' ', 'o', ' '],
+    ]
+    board = game_board(board1)
+    move = capture((1, 2), (3, 0))
+    board.push_or_capture(move, 1)
+    board2 = [
+        [' ', ' ', ' ', 'x', ' ', 'x', ' ', 'x'],
+        [' ', ' ', ' ', ' ', 'x', ' ', 'x', ' '],
+        [' ', ' ', ' ', 'x', ' ', 'x', ' ', 'x'],
+        ['x', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+        [' ', ' ', 'o', ' ', 'o', ' ', 'o', ' '],
+        [' ', 'x', ' ', 'o', ' ', 'o', ' ', 'o'],
+        [' ', ' ', 'o', ' ', 'o', ' ', 'o', ' '],
+    ]
+    assert board.board() == board2
+
+
+def test_push_or_capture_king():
+    board1 = [
+        [' ', ' ', ' ', 'x', ' ', 'x', ' ', 'x'],
+        ['o', ' ', 'x', ' ', 'x', ' ', 'x', ' '],
+        [' ', ' ', ' ', 'x', ' ', 'x', ' ', 'x'],
+        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+        [' ', ' ', 'o', ' ', 'o', ' ', 'o', ' '],
+        [' ', 'x', ' ', 'o', ' ', 'o', ' ', 'o'],
+        [' ', ' ', 'O', ' ', 'o', ' ', 'o', ' '],
+    ]
+    board = game_board(board1)
+    move = capture((7, 2), (5, 0))
+    board.push_or_capture(move, 1)
+    board2 = [
+        [' ', ' ', ' ', 'x', ' ', 'x', ' ', 'x'],
+        ['o', ' ', 'x', ' ', 'x', ' ', 'x', ' '],
+        [' ', ' ', ' ', 'x', ' ', 'x', ' ', 'x'],
+        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+        ['O', ' ', 'o', ' ', 'o', ' ', 'o', ' '],
+        [' ', ' ', ' ', 'o', ' ', 'o', ' ', 'o'],
+        [' ', ' ', ' ', ' ', 'o', ' ', 'o', ' '],
+    ]
+    assert board.board() == board2
+
+
+def test_bad_push_or_capture():
+    board = game_board()
+    move = push((4, 1), (3, 2))
+    with pytest.raises(NoPieceOnTheSquareError):
+        board.push_or_capture(move, 0)
+
+
+def test_make_a_move_push():
+    board = game_board()
+    move = push((5, 0), (4, 1))
+    board.make_a_move(move)
+    board2 = [
+        [' ', 'x', ' ', 'x', ' ', 'x', ' ', 'x'],
+        ['x', ' ', 'x', ' ', 'x', ' ', 'x', ' '],
+        [' ', 'x', ' ', 'x', ' ', 'x', ' ', 'x'],
+        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+        [' ', 'o', ' ', ' ', ' ', ' ', ' ', ' '],
+        [' ', ' ', 'o', ' ', 'o', ' ', 'o', ' '],
+        [' ', 'o', ' ', 'o', ' ', 'o', ' ', 'o'],
+        ['o', ' ', 'o', ' ', 'o', ' ', 'o', ' '],
+    ]
+    assert board.board() == board2
+
+
+def test_make_a_move_capture():
+    board1 = [
+        [' ', ' ', ' ', 'x', ' ', 'x', ' ', 'x'],
+        ['o', ' ', 'x', ' ', 'x', ' ', 'x', ' '],
+        [' ', ' ', ' ', 'x', ' ', 'x', ' ', 'x'],
+        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+        [' ', ' ', 'o', ' ', 'o', ' ', 'o', ' '],
+        [' ', 'x', ' ', 'o', ' ', 'o', ' ', 'o'],
+        [' ', ' ', 'o', ' ', 'o', ' ', 'o', ' '],
+    ]
+    board = game_board(board1)
+    move = capture((7, 2), (5, 0))
+    board.make_a_move(move)
+    board2 = [
+        [' ', ' ', ' ', 'x', ' ', 'x', ' ', 'x'],
+        ['o', ' ', 'x', ' ', 'x', ' ', 'x', ' '],
+        [' ', ' ', ' ', 'x', ' ', 'x', ' ', 'x'],
+        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+        ['o', ' ', 'o', ' ', 'o', ' ', 'o', ' '],
+        [' ', ' ', ' ', 'o', ' ', 'o', ' ', 'o'],
+        [' ', ' ', ' ', ' ', 'o', ' ', 'o', ' '],
+    ]
+    assert board.board() == board2
+
+
+def test_bad_make_a_move():
+    board = game_board()
+    with pytest.raises(NotAMoveError):
+        board.make_a_move('move')
