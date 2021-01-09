98e11eda46beafa2331f1247c9787ac64194413c
pawel <pawelsk.vp.pl@gmial.com>
2021-01-09 19:04:35 +0100

    created file with const for pygame

---
+++ Constants.py
@@ -0,0 +1,13 @@
+red = (255, 0, 0)
+white = (255, 255, 255)
+blue = (0, 0, 255)
+black = (0, 0, 0)
+green = (0, 255, 0)
+
+width, height = 800, 800
+rowCoun, colCoun = 8, 8
+
+sqSize = width // colCoun
+radiusB = sqSize // 2 - 10
+radiusS = radiusB - 20
+fps = 60
