03150ef982e2b35f7a68aa29a0eed5e2d1ce1e9f
pawel <pawelsk.vp.pl@gmial.com>
2021-01-21 23:41:30 +0100

    comments i forgot to add last time

--- players.py
+++ players.py
@@ -33,6 +33,7 @@ class Player():
     def get_move(self, gameboard: 'game_board', turn, WIN):
         """
         get_move method reads coordinates of a move player wants to make
+        and run that tells if ter was a request to close programme
 
         :param gameboard: current state of the game on the board
         :param type: game_board
@@ -41,25 +42,34 @@ class Player():
         :param WIN: window where things can be displayed
         :param type: pygame.display
         """
+        # loop that ensures that function doesn't end to soon,
+        # because programme run out of events
         while True:
+            # basic event loop
             for event in pygame.event.get():
+                # if the window was closed
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     return 0, 0, 0, 0, False
                 else:
+                    # if player is not na AI
                     if self.ai() == 0:
                         if event.type == pygame.MOUSEBUTTONDOWN:
                             pos = pygame.mouse.get_pos()
                             x1, y1 = get_coordinates_from_mouse(pos)
+                            # calculating logical position of the square chosen
                             x = sqSize * x1 + sqSize // 2
                             y = sqSize * y1 + sqSize // 2
+                            # marking selected square
                             pygame.gfxdraw.circle(WIN, y, x, radiusB+2, green)
                             pygame.display.update()
                             x2, y2, run = get_destination()
+                            # unmarking selected square
                             pygame.gfxdraw.circle(WIN, y, x, radiusB+2, black)
                             pygame.display.update()
                             return x1, y1, x2, y2, run
                     else:
+                        # if player is an AI
                         x1, y1, x2, y2 = algorithm(gameboard, turn, self.depth)
                         return x1, y1, x2, y2, True
 
@@ -69,8 +79,12 @@ def get_destination():
     Function get_destination returns to which square the player wants to move
     his piece
     """
+    # loop that ensures that function doesn't end to soon,
+    # because programme run out of events
     while True:
+        # basic event loop
         for event in pygame.event.get():
+            # if the window was closed
             if event.type == pygame.QUIT:
                 pygame.quit()
                 return 0, 0, False
